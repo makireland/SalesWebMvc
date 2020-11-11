f9f543dd6d99ee547d7e1fa6cdc4472437e9daf7
Maksuel <makireland@gmail.com>
2020-11-11 22:36:23 +0000

    Seed Id's Fixed

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -29,14 +29,14 @@ namespace SalesWebMvc.Data
             var department4 = new Department(4, "Book");
 
             var seller1 = new Seller(1, "Maria", "maria@", new DateTime(1989,4,1), 1000.00, department1);
-            var seller2 = new Seller(1, "Pedro", "pedro@", new DateTime(1984,4,1), 1000.00, department3);
-            var seller3 = new Seller(1, "Jose", "jose@", new DateTime(1989,8,1), 1000.00, department4);
-            var seller4 = new Seller(1, "Joao", "joao@", new DateTime(1989,4,11), 1000.00, department2);
+            var seller2 = new Seller(2, "Pedro", "pedro@", new DateTime(1984,4,1), 1000.00, department3);
+            var seller3 = new Seller(3, "Jose", "jose@", new DateTime(1989,8,1), 1000.00, department4);
+            var seller4 = new Seller(4, "Joao", "joao@", new DateTime(1989,4,11), 1000.00, department2);
 
             var salesRecord1 = new SalesRecord(1, new DateTime(2018,6,3), 11000.00, SalesStatus.Billed, seller2 );
-            var salesRecord2 = new SalesRecord(1, new DateTime(2010,7,13), 11000.00, SalesStatus.Pending, seller3 );
-            var salesRecord3 = new SalesRecord(1, new DateTime(2018,12,31), 11000.00, SalesStatus.Canceled, seller4 );
-            var salesRecord4 = new SalesRecord(1, new DateTime(2017,8,23), 11000.00, SalesStatus.Billed, seller1 );
+            var salesRecord2 = new SalesRecord(2, new DateTime(2010,7,13), 11000.00, SalesStatus.Pending, seller3 );
+            var salesRecord3 = new SalesRecord(3, new DateTime(2018,12,31), 11000.00, SalesStatus.Canceled, seller4 );
+            var salesRecord4 = new SalesRecord(4, new DateTime(2017,8,23), 11000.00, SalesStatus.Billed, seller1 );
 
             _context.Department.AddRange(department1, department2, department3, department4);
             _context.Seller.AddRange(seller1, seller2, seller3, seller4);
