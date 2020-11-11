25347617a2dfbc2da8f85ad2dcc3ab9cac50f2ba
Maksuel <makireland@gmail.com>
2020-11-11 00:14:41 +0000

    Creating a ViewModel folder and moved the class ErrorViewModel into this folder and First Model-Controller-View - Department Created

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var departmentList = new List<Department>();
+            departmentList.Add(new Department { Id = 1, Name = "Eletronics"});
+            departmentList.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(departmentList);
+        }
+    }
+}
