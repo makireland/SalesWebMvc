bb8b4e7a690f8efb31ffa65a991cd7b97e7035d8
Maksuel <makireland@gmail.com>
2020-11-15 01:19:30 +0000

    App locale, number and date formatting

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,29 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
-        public string Name  { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DateBirth { get; set; }
-        public double BaseSalary{ get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
